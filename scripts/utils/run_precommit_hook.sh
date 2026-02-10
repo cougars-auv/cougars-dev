ceadfda761e37e71af99a4ef4ce9375f2fb39fe6
Nelson Durrant <snelsondurrant@gmail.com>
2026-02-10 13:57:02 -0700

    Pre-commit fix

--- scripts/utils/run_precommit_hook.sh
+++ scripts/utils/run_precommit_hook.sh
@@ -17,5 +17,4 @@ done
 docker exec cougars-ct /bin/bash -c \
     "source /opt/ros/humble/setup.bash \
     && cd coug_ws \
-    && export AMENT_CPPCHECK_ALLOW_SLOW_VERSIONS=1 \
     && $hook_name ${args[*]} \"\$@\"" -- "${files[@]}"
