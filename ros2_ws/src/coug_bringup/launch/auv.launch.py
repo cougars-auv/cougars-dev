80c5ce69e3380444139dea1cae3ebba8d520ce1f
Nelson Durrant <snelsondurrant@gmail.com>
2026-05-14 16:03:20 -0600

    Added CougUV full EKF option

--- ros2_ws/src/coug_bringup/launch/auv.launch.py
+++ ros2_ws/src/coug_bringup/launch/auv.launch.py
@@ -19,6 +19,7 @@ from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, Grou
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import (
+    AndSubstitution,
     EqualsSubstitution,
     NotEqualsSubstitution,
     LaunchConfiguration,
@@ -76,19 +77,36 @@ def generate_launch_description() -> LaunchDescription:
             "compare": compare,
             "set_origin": set_origin,
         }.items(),
-        condition=IfCondition(NotEqualsSubstitution(auv_ns, "coug2")),
+        condition=IfCondition(
+            AndSubstitution(
+                NotEqualsSubstitution(auv_ns, "coug2_dvl"),
+                NotEqualsSubstitution(auv_ns, "coug2_ekf"),
+            )
+        ),
     )
 
-    coug_fgo_simple_cmd = IncludeLaunchDescription(
+    coug_fgo_dvl_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(coug_fgo_launch_dir, "coug_fgo_simple.launch.py")
+            os.path.join(coug_fgo_launch_dir, "coug_fgo_dvl.launch.py")
         ),
         launch_arguments={
             "use_sim_time": use_sim_time,
             "auv_ns": auv_ns,
             "set_origin": set_origin,
         }.items(),
-        condition=IfCondition(EqualsSubstitution(auv_ns, "coug2")),
+        condition=IfCondition(EqualsSubstitution(auv_ns, "coug2_dvl")),
+    )
+
+    coug_fgo_ekf_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(coug_fgo_launch_dir, "coug_fgo_ekf.launch.py")
+        ),
+        launch_arguments={
+            "use_sim_time": use_sim_time,
+            "auv_ns": auv_ns,
+            "set_origin": set_origin,
+        }.items(),
+        condition=IfCondition(EqualsSubstitution(auv_ns, "coug2_ekf")),
     )
 
     coug_helm_cmd = IncludeLaunchDescription(
@@ -162,7 +180,8 @@ def generate_launch_description() -> LaunchDescription:
                     PushRosNamespace(auv_ns),
                     coug_des_cmd,
                     coug_fgo_cmd,
-                    coug_fgo_simple_cmd,
+                    coug_fgo_dvl_cmd,
+                    coug_fgo_ekf_cmd,
                     coug_helm_cmd,
                     coug_active_fgo_cmd,
                     coug_viz_dvl_cmd,
