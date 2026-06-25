7e8afb79a192c1b7e5743860f9b3cdb89ea8820e
Nelson Durrant <snelsondurrant@gmail.com>
2026-06-24 18:50:42 -0600

    Add school email instead

--- ros2_ws/src/coug_bringup/setup.py
+++ ros2_ws/src/coug_bringup/setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="snelsondurrant",
-    maintainer_email="snelsondurrant@gmail.com",
+    maintainer_email="snelsond@byu.edu",
     description="Full-system launch files for the CougUV",
     license="Apache-2.0",
     extras_require={
