c83a72477eac19d860de809b3a469487aaa62ca9
Nelson Durrant <snelsondurrant@gmail.com>
2026-07-15 11:49:13 -0600

    Fix for script exits

--- scripts/key_teleop.sh
+++ scripts/key_teleop.sh
@@ -4,7 +4,7 @@ set -e
 source ${OVERLAY_WS}/install/setup.bash
 
 # --- Selection ---
-# agent_ns=$(basename -a "${CONFIG_DIR}"/*_params.yaml | sed 's/_params.yaml$//' | sort | gum filter --placeholder "Select an agent to drive..." || exit 0)
+# agent_ns=$(basename -a "${CONFIG_DIR}"/*_params.yaml | sed 's/_params.yaml$//' | sort | gum filter --placeholder "Select an agent to drive...") || exit 0
 # [ -z "${agent_ns}" ] && exit 0
 
 agent_ns="blue1sim"
