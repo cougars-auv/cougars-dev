1064af0382f3991460e3da768682f731ffce4977
Nelson Durrant <snelsondurrant@gmail.com>
2026-07-28 10:47:37 -0600

    Add WAM-V keyboard controls

--- scripts/key_teleop.sh
+++ scripts/key_teleop.sh
@@ -4,10 +4,8 @@ set -e
 source ${OVERLAY_WS}/install/setup.bash
 
 # --- Selection ---
-# agent_ns=$(basename -a "${CONFIG_DIR}"/*_params.yaml | sed 's/_params.yaml$//' | sort | gum filter --placeholder "Select an agent to drive...") || exit 0
-# [ -z "${agent_ns}" ] && exit 0
-
-agent_ns="blue1sim"
+agent_ns=$(basename -a "${CONFIG_DIR}"/*_params.yaml | sed 's/_params.yaml$//' | sort | gum filter --placeholder "Select an agent to drive...") || exit 0
+[ -z "${agent_ns}" ] && exit 0
 
 # --- Launch ---
 ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args \
