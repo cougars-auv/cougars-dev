fff170810c6e319b48bbb1639a9cb22e408d99e4
Nelson Durrant <snelsondurrant@gmail.com>
2026-08-31 14:00:08 -0600

    Move tmuxp into config

--- connect.sh
+++ connect.sh
@@ -11,5 +11,5 @@ cd "$(dirname "$0")"
 agent_ns="$1"
 
 ssh frostlab@"${agent_ns}".local \
-  "tmux has-session -t couguv-launch 2>/dev/null || tmuxp load -d ~/cougars-dev/.tmuxp/couguv-launch.yaml"
-mosh frostlab@"${agent_ns}".local -- tmux attach -t couguv-launch
+  "tmux has-session -t couguv 2>/dev/null || tmuxp load -d ~/cougars-dev/config/tmuxp/couguv.yaml"
+mosh frostlab@"${agent_ns}".local -- tmux attach -t couguv
