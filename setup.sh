35685034351c079284c9fd2e8ba53bcb1f0efd30
Nelson Durrant <snelsondurrant@gmail.com>
2026-08-31 14:02:10 -0600

    Quick update

--- setup.sh
+++ setup.sh
@@ -33,9 +33,8 @@ fi
 sed 's|git@github.com:|https://github.com/|g' runtime.repos | vcs import ros2_ws/src
 vcs custom -n --git --args submodule update --init --recursive
 
-for repo in ros2_ws/src/*/; do
-  repo="${repo%/}"
-  [ -d "${repo}/.git" ] || continue
+for git_dir in $(find ros2_ws/src -maxdepth 2 -name .git -prune); do
+  repo="$(dirname "${git_dir}")"
   git -C "${repo}" remote add base "git://${ip}/cougars-dev/${repo}" 2>/dev/null || \
     git -C "${repo}" remote set-url base "git://${ip}/cougars-dev/${repo}"
 done
