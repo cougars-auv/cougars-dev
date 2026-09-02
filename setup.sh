9a60ce9804a3b9d77698ff8ccd1049d828818800
Nelson Durrant <snelsondurrant@gmail.com>
2026-09-02 15:53:19 -0600

    Use SSH still

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ if [ ! -f "config/${agent_ns}_params.yaml" ]; then
   exit 1
 fi
 
-vcs import ros2_ws/src < runtime.repos
+sed 's|git@github.com:|https://github.com/|g' runtime.repos | vcs import ros2_ws/src
 vcs custom -n --git --args submodule update --init --recursive
 
 for git_dir in $(find ros2_ws/src -maxdepth 2 -name .git -prune); do
