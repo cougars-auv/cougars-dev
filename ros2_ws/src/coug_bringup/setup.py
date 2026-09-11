69003cfbdba28d21ce1173fd6162bae2dfc42661
Nelson Durrant <snelsondurrant@gmail.com>
2026-09-11 14:42:27 -0600

    Simpler versioning

--- ros2_ws/src/coug_bringup/setup.py
+++ ros2_ws/src/coug_bringup/setup.py
@@ -7,7 +7,6 @@ package_name = "coug_bringup"
 
 setup(
     name=package_name,
-    version="0.0.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
@@ -15,12 +14,7 @@ setup(
         (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
         (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
     ],
-    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer="snelsondurrant",
-    maintainer_email="snelsond@byu.edu",
-    description="System-wide ROS 2 launch configurations.",
-    license="Apache-2.0",
     extras_require={
         "test": [
             "pytest",
