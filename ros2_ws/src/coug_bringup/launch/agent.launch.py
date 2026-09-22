d0955b5f283758576e8aabb842e55c0451b7bb0e
Nelson Durrant <snelsondurrant@gmail.com>
2026-09-21 19:25:14 -0600

    Add rover nav2 config to WAM-V

--- ros2_ws/src/coug_bringup/launch/agent.launch.py
+++ ros2_ws/src/coug_bringup/launch/agent.launch.py
@@ -186,7 +186,7 @@ def generate_launch_description() -> LaunchDescription:
             "agent_ns": agent_ns,
             "scenario_param_file": scenario_param_file,
         }.items(),
-        condition=IfCondition(is_agent(agent_ns, "rover1gz", "rover2gz")),
+        condition=IfCondition(is_agent(agent_ns, "rover1gz", "rover2gz", "wamv1holo")),
     )
 
     coug_visual_dvl_launch = IncludeLaunchDescription(
